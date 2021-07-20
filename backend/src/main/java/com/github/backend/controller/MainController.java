ba8f8eec4dbec658b359c621171375efc8824bcc
Tan Phung <tan.phung@vexere.com>
2021-07-20 23:49:35 +0700

    commit

---
+++ backend/src/main/java/com/github/backend/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.github.backend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class MainController {
+	private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
+
+	public static final String HELLO_TEXT = "Hello from Spring Boot Backend";
+	public static final String SECURED_TEXT = "Hello from the secured resource";
+
+	@CrossOrigin(origins = "http://localhost:8080")
+	// Set to enable CORS for request for resources from localhost:8080, the origin is different from the one in the cookie it attache to
+	@ResponseBody
+	@RequestMapping(path = "/hello")
+	public String sayHello() {
+		LOG.info("GET called on '/hello' resource");
+		return HELLO_TEXT;
+	}
+}
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <button class="search_button" @click="callRestService()">
+      CALL Spring Boot REST backend service
+    </button>
+
+    <h3>Response:</h3>
+    <p>{{ response }}</p>
+
+    <h3>Errors:</h3>
+    <p>{{ errors }}</p>
+
+  </div>
+</template>
+
+<script>
+import { AXIOS } from '../utils/http-common';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      response: [],
+      errors: [],
+    };
+  },
+  methods: {
+    callRestService() {
+      AXIOS.get('hello').then((response) => {
+        console.log(123123123);
+        console.log(response);
+        this.response = response.data;
+      }).catch((e) => {
+        this.errors.push(e);
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
